43e28c4546f96ebf81f4926500e2f736192e496b
williamxiron <69971087+williamxiron@users.noreply.github.com>
2020-09-26 14:38:11 +1000

    Update scrollanimations.js

--- scripts/scrollanimations.js
+++ scripts/scrollanimations.js
@@ -3,15 +3,15 @@ gsap.registerPlugin(ScrollTrigger)
 function init(){
 
     gsap.from('.aboutdiv', {opacity: 0, y: 100, duration: 1, scrollTrigger: {
-        trigger:'.aboutdiv', markers: true
+        trigger:'.aboutdiv', markers: false
     }})
 
     gsap.from('.projects h1', {opacity: 0, y: 100, duration: 1, scrollTrigger: {
-        trigger:'.projects h1', markers: true
+        trigger:'.projects h1', markers: false
     }})
 
     gsap.from('.projects .container', {opacity: 0, y: 100, duration: 1, scrollTrigger: {
-        trigger:'.projects .container', markers: true
+        trigger:'.projects .container', markers: false
     }})
 }
 
