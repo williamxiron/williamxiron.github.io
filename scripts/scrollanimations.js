dcfd94445b67456387dc6ab6a3a74b6cc74e06ee
williamxiron <69971087+williamxiron@users.noreply.github.com>
2020-09-26 14:32:50 +1000

    Add files via upload

---
+++ scripts/scrollanimations.js
@@ -0,0 +1,22 @@
+gsap.registerPlugin(ScrollTrigger)
+
+function init(){
+
+    gsap.from('.aboutdiv', {opacity: 0, y: 100, duration: 1, scrollTrigger: {
+        trigger:'.aboutdiv', markers: true
+    }})
+
+    gsap.from('.projects h1', {opacity: 0, y: 100, duration: 1, scrollTrigger: {
+        trigger:'.projects h1', markers: true
+    }})
+
+    gsap.from('.projects .container', {opacity: 0, y: 100, duration: 1, scrollTrigger: {
+        trigger:'.projects .container', markers: true
+    }})
+}
+
+
+
+window.addEventListener('load', function(){
+    init()
+})
